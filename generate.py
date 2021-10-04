fc9ec8b297171a2e721ea0c0a021a6647c8991fb
Will Merrill <william.merrill@yale.edu>
2021-10-04 14:30:41 -0400

    Tests, debug quantifier generation.

--- generate.py
+++ generate.py
@@ -4,7 +4,7 @@ from argparse import ArgumentParser
 from tqdm import trange
 import logging
 import torch
-from random import randint
+import random
 
 from src.quantifier.syntax import QuantifierSyntax
 from src.quantifier.semantics import QuantifierSemantics, QuantifierWorld
@@ -19,25 +19,29 @@ log.setLevel(logging.INFO)
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument("-n", "--n_docs", type=int, default=10000)
-    parser.add_argument("--doc_len", type=int, default=5)
-    parser.add_argument("--n_items", type=int, default=5)
-    parser.add_argument("--n_predicates", type=int, default=2)
-    parser.add_argument("--eval", action="store_true")
-    parser.add_argument("--temp", type=float, default=1.)
-    parser.add_argument("--cost", type=float, default=1/3)
-    parser.add_argument("--no_updates", action="store_true")
-    parser.add_argument("--only_true", action="store_true")
+    parser.add_argument("-n", "--n_docs", type=int, default=10000, help="Number of total documents (training examples) to sample.")
+    parser.add_argument("--doc_len", type=int, default=5, help="Number of sentences per document.")
+    parser.add_argument("--n_items", type=int, default=5, help="Number of entities.")
+    parser.add_argument("--n_predicates", type=int, default=2, help="Number of predicates.")
+    parser.add_argument("--temp", type=float, default=1., help="Temperature parameter in RSA model. Controls rationality.")
+    parser.add_argument("--cost", type=float, default=1/3, help="Cost per token.")
+    parser.add_argument("--no_updates", action="store_true", help="Don't update speaker belief model.")
+    parser.add_argument("--only_true", action="store_true", help="Only generate true statements.")
+    parser.add_argument("--test", action="store_true", help="Generate test set instead of train.")
+    parser.add_argument("--seed", type=int, default=2, help="Fixed random seed for data generation.")
     return parser.parse_args()
 
 
 @torch.no_grad()
 def main(args):
+    random.seed(args.seed)
+    torch.manual_seed(args.seed)
+
     worlds = list(QuantifierWorld.generate_all(args.n_items, args.n_predicates))
     syntax = QuantifierSyntax(args.n_predicates)
     semantics = QuantifierSemantics(worlds)
 
-    utterances = list(syntax.generate())
+    utterances = list(syntax.generate(train=not args.test))
     truth_values = torch.tensor([[semantics.evaluate(e, w) for w in worlds] for e in utterances])
     costs = torch.tensor([args.cost * len(u) for u in utterances])
     belief_state = torch.zeros(len(worlds))
@@ -46,7 +50,7 @@ def main(args):
     agent = RationalAgent(rsa, temp=args.temp) if not args.only_true else OnlyTrueAgent(rsa)
 
     for _ in trange(args.n_docs):
-        belief_state[randint(0, len(worlds) - 1)] = 1
+        belief_state[random.randint(0, len(worlds) - 1)] = 1
         document = agent.sample_monologue(belief_state, length=args.doc_len, update_prior=not args.no_updates)
         sentences = [" ".join(str(x) for x in sent) for sent in document if sent]
         serialized = " ; ".join(sent for sent in sentences)
