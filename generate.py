55f4e3d0b57c2234ac5e078f0fb0545b23455365
Alex Warstadt <alexwarstadt@gmail.com>
2022-04-07 15:34:21 -0400

    debug generation code, add powerset

--- generate.py
+++ generate.py
@@ -9,9 +9,10 @@ from itertools import product
 
 from src.quantifier.syntax import SimpleQuantifierSyntax
 from src.quantifier.semantics import SimpleQuantifierSemantics, SimpleQuantifierWorld
-from src.quantifier.serialize import to_string
+from src.quantifier.serialize import to_string as q_to_string
+from src.powerset.syntax import PowersetSyntax
+from src.powerset.semantics import PowersetSemantics
 from src.rational_speech import RationalAgent, RationalSpeechActs
-# from src.only_true_speech import OnlyTrueAgent
 
 
 logging.basicConfig()
@@ -22,15 +23,16 @@ log.setLevel(logging.INFO)
 def parse_args():
     parser = ArgumentParser()
     # parser.add_argument("agent", type=str, choices=["true", "rsa", "eval"], help="Agent model to use, or train/test for eval dataset.")
-    parser.add_argument("--task", type=str, choices=["quantifier", "arithmetic"], default="quantifier")
+    parser.add_argument("--task", type=str, choices=["quantifier", "powerset"], default="quantifier")
     parser.add_argument("-n", "--n_docs", type=int, default=10000, help="Number of total documents (training examples) to sample.")
     parser.add_argument("--n_sents", type=int, default=10, help="Number of sentences per document.")
     parser.add_argument("--n_items", type=int, default=5, help="Number of entities.")
     # parser.add_argument("--n_predicates", type=int, default=2, help="Number of predicates.")
-    parser.add_argument("--cost", type=float, default=0, help="Cost per token.")
+    parser.add_argument("--cost", type=float, default=1, help="Cost per token.")
     parser.add_argument("--seed", type=int, default=2, help="Fixed random seed for data generation.")
     parser.add_argument("--temp", type=float, default=1., help="Temperature parameter in RSA model. Controls rationality.")
     parser.add_argument("--depth", type=int, default=1, help="Depth of RSA process, in the notation of the paper.")
+    parser.add_argument("--vanilla", action="store_true", help="Whether to use vanilla RSA. Default if not set is to use vanilla anyway.")
     parser.add_argument("--noisy", action="store_true", help="Whether to use cRSA instead of normal RSA.")
     parser.add_argument("--dependent", action="store_true", help="Should second sentence depend on the first?")
     parser.add_argument("--eval", action="store_true", help="Create evaluation data.")
@@ -47,11 +49,16 @@ def main(args):
         syntax = SimpleQuantifierSyntax()
         worlds = list(SimpleQuantifierWorld.generate_all(args.n_items))
         semantics = SimpleQuantifierSemantics(worlds)
-    elif args.task == "arithmetic":
-        pass
+        to_string = q_to_string
+    elif args.task == "powerset":
+        syntax = PowersetSyntax(args.n_items)
+        semantics = PowersetSemantics()
+        worlds = [w for w in range(args.n_items)]
+        to_string = lambda prop: "".join(str(x) for x in prop)
     else:
         raise NotImplementedError(f"Unknown task: {args.task}.")
     utterances = list(syntax.generate())
+    costs = torch.tensor([args.cost * syntax.get_cost(u) for u in utterances])
 
     if args.eval:
         # Generate a semantic evaluation suite.
@@ -62,26 +69,18 @@ def main(args):
         return
 
     truth_values = torch.tensor([[semantics.evaluate(e, w) for w in worlds] for e in utterances])
-    costs = torch.tensor([args.cost * len(u) for u in utterances])
     errors = torch.tensor([.1 for _ in utterances])
     rsa = RationalSpeechActs(utterances, truth_values, costs, errors)
     agent = RationalAgent(rsa, temp=args.temp, depth=args.depth, noisy=args.noisy, conditional_independence=not args.dependent)
-
-    # for _ in tqdm.trange(args.n_docs):
-    #     world = random.randint(0, len(worlds) - 1)
-    #     sent1 = agent.speak(world)
-    #     sent2 = agent.speak(world, context=[sent1])
-    #     doc = sent1 + sent2
-    #     serialized = " ".join(x for x in doc if x)
-    #     print(serialized)
 
     for _ in tqdm.trange(args.n_docs):
         world = random.randint(0, len(worlds) - 1)
         context = []
         for _ in range(args.n_sents):
-            sent = agent.speak(world)
-            context = context + sent
-        serialized = " ".join(x for x in context if x)
+            curr_context = None if context == [] else context
+            sent = agent.speak(world, context=curr_context)
+            context.append(sent)
+        serialized = " ".join(to_string(x) for x in context if not syntax.is_empty(x))
         print(serialized)
 
 
